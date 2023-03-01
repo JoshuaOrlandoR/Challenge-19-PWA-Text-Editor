a985e6a6d8eb8f5864f76ddae8c6db17e8bea355
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-03-01 10:13:19 -0500

    Changed service worker expiration plugin duration

--- client/src-sw.js
+++ client/src-sw.js
@@ -37,7 +37,7 @@ registerRoute(
       }),
       new ExpirationPlugin({
         maxEntries: 60,
-        maxAgeSeconds: 604800 //one week
+        maxAgeSeconds:  30 * 24 * 60 * 60, 
       })
     ]
   })
